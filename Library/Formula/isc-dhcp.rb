cbf628bd4ab0ecb4f37b60a9e87fcd481601e49e
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2010-08-24 08:16:44 +0300

    Added ISC DHCP distribution

--- Library/Formula/isc-dhcp.rb
+++ Library/Formula/isc-dhcp.rb
@@ -4,12 +4,11 @@ class IscDhcp <Formula
   url 'ftp://ftp.isc.org/isc/dhcp/dhcp-3.1.3.tar.gz'
   homepage 'http://www.isc.org/software/dhcp'
   md5 '6ee8af8b283c95b3b4db5e88b6dd9a26'
+  
+  version '3.1.3'
 
   def install
-    inreplace "Makefile.conf" do |s|
-      s.gsub! "/usr", ""
-      s.gsub! %r[man/cat(\d)], "man/man\\1"
-    end
+    inreplace "Makefile.conf", "/usr", ""
 
     system "./configure"
     system "make"
